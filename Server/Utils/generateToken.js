574539d25f85d7b123c773a684e993d457fe0083
niraj436 <nirajkhadka634@gmail.com>
2024-11-12 17:29:45 +0545

    same site

--- Server/Utils/generateToken.js
+++ Server/Utils/generateToken.js
@@ -1,14 +1,15 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 const generateTokenandSetCookie = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_KEY, {
-    expiresIn: "15d",
-  });
-  res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in mm
-    httpOnly: true,
-    path:"/",
-  });
+	const token = jwt.sign({ userId }, process.env.JWT_KEY, {
+		expiresIn: '15d',
+	});
+	res.cookie('jwt', token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in mm
+		httpOnly: true,
+		sameSite: 'none',
+		path: '/',
+	});
 };
 
 export default generateTokenandSetCookie;
--- Server/index.js
+++ Server/index.js
@@ -1,36 +1,34 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors"
-import morgan  from "morgan";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
-import authRoutes from "./Router/authRoute.js";
-import messageRoutes from "./Router/messageRoute.js";
-import UserRoutes from "./Router/userRoute.js";
-import connectToMoongose from "./DbConnection/MongoConn.js";
+import authRoutes from './Router/authRoute.js';
+import messageRoutes from './Router/messageRoute.js';
+import UserRoutes from './Router/userRoute.js';
+import connectToMoongose from './DbConnection/MongoConn.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000; 
+const PORT = process.env.PORT || 8000;
 
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: true,
+		credentials: true,
+	})
+);
 
-app.use(morgan("dev"));
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
-
-
- 
-
-app.use("/auth", authRoutes);
-app.use("/messages", messageRoutes);
-app.use("/users", UserRoutes);
+app.use('/auth', authRoutes);
+app.use('/messages', messageRoutes);
+app.use('/users', UserRoutes);
 
 app.listen(PORT, () => {
-  connectToMoongose();
-  console.log(`Server running at ${PORT}`);
+	connectToMoongose();
+	console.log(`Server running at ${PORT}`);
 });
